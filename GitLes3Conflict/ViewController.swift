4b280f2b0ec5875c1f09dd4921eb691da2ca61c8
Sonnuy12 <kazanskij516@gmail.com>
2024-12-11 16:28:00 +0300

    изменение в ветки main

--- GitLes3Conflict/ViewController.swift
+++ GitLes3Conflict/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var forConflict: String = "Это main ветка тут"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        title = forConflict
     }
 
 
